9994e983854870452c8a12d44aee53a82754e9a9
lex-666 <lexart666@gmail.com>
2022-11-26 21:59:32 +0200

    first commit

---
+++ HelloNode/app.js
@@ -0,0 +1,14 @@
+function getLargestExpressionResult (10, 5){
+  let result = a + b;
+  if (result < (a - b)) {
+      return result = a - b;
+  }
+  if (result < (a * b)) {
+      return result = a * b;
+  }
+  if (result < (a / b)) {
+      return result = a / b;
+    }
+  return result;
+}
+console.log(result);
